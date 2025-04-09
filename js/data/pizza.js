17df2922c39a18f4231ce5a151b734b2dfe8e373
KristinaKripke <kristina.kripke@gmail.com>
2025-04-09 16:01:59 +0300

    dom

--- js/data/pizza.js
+++ js/data/pizza.js
@@ -1,36 +1,36 @@
 export const foodList = [
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: 'Margarita',
         price: 1000,
         availability: 4,
     },
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: 'Havaju',
         price: 890,
         availability: 18,
     },
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: '4 sezonu',
         price: 1299,
         availability: 1,
     },
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: 'Ham & Mushrooms',
         price: 2200,
         availability: 5,
     },
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: '4 suriu',
         price: 1313,
         availability: 13,
     },
     {
-        photo: 'pizza.png',
+        photo: 'pizza.webp',
         name: 'Mafia',
         price: 89,
         availability: 0,
